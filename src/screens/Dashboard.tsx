25a4ba3edf158cccbd600ad86da6df2d1ef2b698
Caio Mori <chmori@viptech.com.br>
2022-11-22 02:43:59 -0300

    feat(dashboard): add components to dashboard

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -1,11 +1,44 @@
+import { Box, Grid, Stack } from "@mui/material";
 import Header from "../components/Header/Header";
+import { BoxContainer, StackContainer } from "./DashboardBase";
+import useDataInfo from "../hooks/useDataInfo";
+import { useEffect, useState } from "react";
+import DataReceiver from "../components/DataReceiver/DataReceiver";
+import device from "../mocks/device";
+import getRandomValue from "../utils/getRandomValue";
+import ActivityMonitor from "../components/ActivityMonitor/ActivityMonitor";
 
 function Dashboard() {
+  const dataInfo = useDataInfo();
+  const [mainDevice, setMainDevice] = useState(device);
+
+  useEffect(() => {
+    if (dataInfo.timestamp) {
+      device.variables.forEach((variable) => {
+        const randomValue = getRandomValue(0, 100);
+        variable.value = randomValue;
+        variable.histories.push({
+          timestamp: dataInfo.timestamp!,
+          value: randomValue,
+        });
+      });
+      setMainDevice(JSON.parse(JSON.stringify(device)));
+    }
+  }, [dataInfo.timestamp]);
+
   return (
-    <div>
+    <Box sx={BoxContainer}>
       <Header />
-      <h1>Dashboard</h1>
-    </div>
+      <Stack direction="column" sx={StackContainer}>
+        <ActivityMonitor
+          active={dataInfo.active}
+          toggleInterval={dataInfo.toggleInterval}
+        />
+        <Grid container maxWidth="100vw" spacing={2}>
+          <DataReceiver device={mainDevice} isLoading={dataInfo.loading} />
+        </Grid>
+      </Stack>
+    </Box>
   );
 }
 
---
+++ src/screens/DashboardBase.ts
@@ -0,0 +1,12 @@
+export const BoxContainer = {
+  display: "flex",
+  flexDirection: "column",
+  flex: 1,
+  minHeight: "100vh",
+  backgroundColor: "lightgray",
+};
+
+export const StackContainer = {
+  py: "1vh",
+  px: "2vh",
+};
